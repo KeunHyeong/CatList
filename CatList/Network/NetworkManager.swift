8f7ba91d05692c78cbeebd50ca42c1053f0b9929
keunhyeong <rmsgudxx@gmail.com>
2025-03-29 20:13:53 +0900

    [ADD] online, offline체크하는 NetworkManager추가

---
+++ CatList/Network/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  CatList
+//
+//  Created by 장근형 on 3/29/25.
+//
+
+import Foundation
+import Network
+
+class NetworkManager: ObservableObject {
+    @Published var isOnline: Bool = false
+    private let monitor = NWPathMonitor()
+    
+    static let shared = NetworkManager()
+    
+    init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isOnline = path.status == .satisfied
+                print("Network manager pathUPdateHandler \(String(describing: self?.isOnline))")
+            }
+        }
+        
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        monitor.start(queue: queue)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.isOnline = self.monitor.currentPath.status == .satisfied
+            print("Network manager currentPath.status \(self.isOnline)")
+        }
+    }
+}
